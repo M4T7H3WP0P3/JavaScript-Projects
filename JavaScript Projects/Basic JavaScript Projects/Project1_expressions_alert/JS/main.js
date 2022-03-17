4824ad73eb1964c31961b86bec02e3bf2194c368
M4T7H3WP0P3 <99054175+M4T7H3WP0P3@users.noreply.github.com>
2022-03-17 13:06:04 -0600

    Add files via upload

---
+++ JavaScript Projects/Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+var A = "This is Matthew\'s Assignment"
+
+window.alert(A);
+
+var B = "\"Be who you are and say what you feel," + " because those who mind don\'t matter and those who matter don\'t mind.\"" + " -Dr. Seuss"
+
+document.write(B)
+
+var Family = "The Popes", Dad = "Matthew", Son1 = "Christopher", Son2 = "Lincoln", Son3 = "Tyson"
+document.write(Dad)
+
+var blues = "I have the blues."; //Defines a variable and gives it a string value
+var blues = blues.fontcolor("blue"); //Adds font color to text of variable. 
+document.write(blues);
+
+document.write(4+5); //Expression that adds 4 and 5 together
